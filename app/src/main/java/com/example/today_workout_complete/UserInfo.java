dd23021d51525d9a4de30e09c007d2a1ffe08c77
ans9971 <ans9971@naver.com>
2022-11-02 17:38:40 +0900

    .

--- app/src/main/java/com/example/today_workout_complete/UserInfo.java
+++ app/src/main/java/com/example/today_workout_complete/UserInfo.java
@@ -6,37 +6,37 @@ public class UserInfo {
 
 
 
-//
-//    @SerializedName("nickname")
-//    private String nickname;
-//
-//    public String getNickname() {
-//        return nickname;
-//    }
-//
-//    public String getEmg_data_path() {
-//        return emg_data_path;
-//    }
-//
-//
-//    @SerializedName("emg_data_path")
-//    private String emg_data_path;
-//
-//
-//
-//    public String getCreation_datetime() {
-//        return creation_datetime;
-//    }
-//
-//
-//
-//    @SerializedName("creation_datetime")
-//    public String creation_datetime;
-//
-//
-//    public UserInfo(String nickname) {
-//        this.nickname = nickname;
-//    }
+
+    @SerializedName("nickname")
+    private String nickname;
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public String getEmg_data_path() {
+        return emg_data_path;
+    }
+
+
+    @SerializedName("emg_data_path")
+    private String emg_data_path;
+
+
+
+    public String getCreation_datetime() {
+        return creation_datetime;
+    }
+
+
+
+    @SerializedName("creation_datetime")
+    public String creation_datetime;
+
+
+    public UserInfo(String nickname) {
+        this.nickname = nickname;
+    }
 
 
 
